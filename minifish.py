eeaad382ad12762d3e3b521bbbdf9c3a5b9c3779
piotr-lewandowski <lewandowskipiotr98@gmail.com>
2024-04-22 13:03:12 +0200

    Some fixes to minifish

--- minifish.py
+++ minifish.py
@@ -9,7 +9,7 @@ from Crypto.Util.number import bytes_to_long, long_to_bytes
 def xor(a, b):
     return [x ^ y for x, y in zip(a, b)]
 
-class MINIFish:
+class MiniFish:
     def hextoint(self, hex):
         return int(hex, 16)
 
@@ -31,17 +31,17 @@ class MINIFish:
         for i in range(4):
             self.sboxes[i] = [None] * 256
             for j in range(256):
-                self.sboxes[i][j] = res[keys_end + i * 256 * 8 + j * 8]
+                self.sboxes[i][j] = res[keys_end + i * 256 * 2 + j * 2: keys_end + i * 256 * 2 + j * 2 + 2]
                 self.sboxes[i][j] = self.hextoint(self.sboxes[i][j])
 
     def feistel(self, x):
-        # print(len(bin(x)) - 2)
-        l = x >> 32
-        r = x & 0xFFFFFFFF
+        print("-----------")
+        l = x >> 32 & 0xFFFF
+        r = x & 0xFFFF
 
         for i in range(16):
             l = l ^ self.round_keys[i]
-            l1 = l >> 24
+            l1 = l >> 24 & 0xFF
             l2 = (l >> 16) & 0xFF
             l3 = (l >> 8) & 0xFF
             l4 = l & 0xFF
@@ -50,8 +50,10 @@ class MINIFish:
             l2 = self.sboxes[1][l2]
             l3 = self.sboxes[2][l3]
             l4 = self.sboxes[3][l4]
+            print(l1, l2, l3, l4)
 
             tmp = self.feistel_mul(l1, l2) ^ self.feistel_mul(l3, l4)
+            # print(len(bin(tmp)) - 2)
             r = r ^ tmp
 
             (l, r) = (r, l)
@@ -68,24 +70,22 @@ class MINIFish:
         bin_len = len(bin(replicated)) - 2
 
         for i in range(18):
-            to_xor = replicated >> (bin_len - 32 * (i + 1)) & 0xFFFFFFFF
+            to_xor = replicated >> (bin_len - 32 * (i + 1)) & 0xFF
             self.round_keys[i] = self.round_keys[i] ^ to_xor
 
         t = 0
 
         for i in range(0, 18, 2):
             t = self.feistel(t)
-            self.round_keys[i] = t >> 32
-            self.round_keys[i + 1] = t & 0xFFFFFFFF
+            self.round_keys[i] = t >> 32 & 0xFFFF
+            self.round_keys[i + 1] = t & 0xFFFF
 
         for i in range(0,4):
             for j in range(0,256, 2):
                 t = self.feistel(t)
-                self.sboxes[i][j] = t >> 32
-                self.sboxes[i][j + 1] = t & 0xFFFFFFFF
+                self.sboxes[i][j] = t >> 32 & 0xFF
+                self.sboxes[i][j + 1] = t & 0xFF
 
-        # =0
-                
     def gf_mul(self, a, b, m):
         p = 0
         while a > 0:
@@ -102,10 +102,13 @@ class MINIFish:
     def deg(self, x):
         return len(bin(x)) - 3
 
-    def galois_mul(self, a, b):
-        return self.gf_mul(a,b,340282366920938463463374607431768211591)
-
     def feistel_mul(self, a, b):
+        # print(len(bin(a)), len(bin(b)))
+        x = self.gf_mul(a,b,340282366920938463463374607431768211591)
+        # print(len(bin(x)))
+        return x
+
+    def galois_mul(self, a, b):
         return self.gf_mul(a,b,4295000729)
 
     def encrypt(self, plaintext, aad, iv):
@@ -137,31 +140,15 @@ class MINIFish:
         return (ciphertext_bytes, tag)
 
 
-# def __main__():
-    # k = 24691357820222427
-    # iv = 98765431280889708
-    # p = 4
-    # aad = 31
-    # fish = MINIFish(k)
+if __name__ == "__main__":
+    key = b'klucz'
+    iv = b'0123'
+    AAD = b'Karol Kasia Oskar'
 
-key = b'klucz'
-iv = b'0123456789AB'
-AAD = b'Karol Kasia Oskar'
+    text1 = b'matematyka jest super'
 
-text1 = b'\xa0\xddW\xfe\xc1\xa5N\xc0Y\xf2q\xea\x18U\x13\xce'
-
-fish = MINIFish(bytes_to_long(key))
-fish.init_keys()
-(cipher, tag) = fish.encrypt(bytes_to_long(text1), bytes_to_long(AAD), bytes_to_long(iv))
-print(cipher)
-print(tag)
-    # gcm ( key , text1 , iv , AAD ) == [ b’\xf3\xc9\ xd8O \x9a\xb8\xb0\x03 >DZ\xea {\ xe7 <\ xca ’,230564314925205440153112389334078896669 ,b’Karol Kasia Oskar ’]
-
-#  text2 = b’matematyka jest super ’
-#  gcm ( key , text2 , iv , AAD ) ==
-#  [ b’>u\xfb\ xd46 |\ x8a\xba\x0c\xd7\ x0bj \x06\xc1[$ a\xff\xd4)\x1d\xfe\ xc2g \←-
-# xb6 +\ x00c \xb2 /\ x04 ’,
-#  139928700887018820760292109760219419355 ,
-#  b’Karol Kasia Oskar ’]
-
-# cipher = AES.new(key, AES.MODE_ECB)
+    fish = MiniFish(bytes_to_long(key))
+    fish.init_keys()
+    (cipher, tag) = fish.encrypt(bytes_to_long(text1), bytes_to_long(AAD), bytes_to_long(iv))
+    print(cipher)
+    print(tag)
